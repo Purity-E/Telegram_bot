a3fbe198959c2545d7d0b2e85a9e7d4dd8abd61b
Purity-E <purityeverniter@gmail.com>
2021-03-11 16:47:32 +0300

    New changes

--- podii_bot.py
+++ podii_bot.py
@@ -6,53 +6,94 @@ import logging
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, CallbackContext,MessageHandler, Filters, CallbackQueryHandler,ConversationHandler
-# enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-logger = logging.getLogger(__name__)
+# creating a user dictionaries
+data = {'name': "", 'age': "", 'gender': "", 'attending':""}
+# --- states use in conversation ---
+
+NAME = 1
+AGE = 2
+GENDER = 3
+ATTENDING = 4
 # getting my token from env file
 load_dotenv("D:/Purity/Podii_bot/.env")
 Token = os.getenv('TOKEN')
 # This function replies with 'Hello <user.first_name>'
-def start(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context: CallbackContext) -> int:
+    global data # to assign new dictionary to external/global variable
+     # create new empty dictionary
+    data = {'name': "", 'age': "", 'gender': "", 'attending':""}
     update.message.reply_text(f'Hello {update.effective_user.first_name} I am a bot. \n'
                               'It is nice to meet you.\n'
-                              'Send /rsvp to proceed otherwise send /cancel to end this conversation')
-start_handler = CommandHandler('start', start)   
-# This function asks whether you'll attend the event or not
-def rsvp(update: Update, context: CallbackContext) -> int:
+                              'Send /cancel to end this conversation but to preceed please write your full names')
+     # next state in conversation 
+    return NAME                          
+def get_name(update: Update, context: CallbackContext) -> int:
+    data['name'] = update.message.text 
+    update.message.reply_text('Please write your age')
+    # next state in conversation 
+    return AGE
+def get_age(update: Update, context: CallbackContext) -> int:
+    data['age'] = update.message.text
+    reply_keyboard = [['Male', 'Female']]
+    update.message.reply_text(
+        'What is your gender?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
+    return GENDER 
+def get_gender(update: Update, context: CallbackContext) -> int:
+    data['gender'] = update.message.text
     reply_keyboard = [['Yes', 'No']]
-    update.message.reply_text(f' {update.effective_user.first_name} will you come to the event?',
-                             reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
-                             )
-rsvp_handler = CommandHandler('rsvp', rsvp)
+    update.message.reply_text(
+        'Will you be attending our event?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
+    return ATTENDING
+def cancel(update: Update, context: CallbackContext) -> int:
+    data['attending'] = update.message.text
+    update.message.reply_text(
+        'Bye! Thank you so much for your time.', reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
+# creating coversation
+my_conversation_handler = ConversationHandler(
+   entry_points=[CommandHandler('start', start)],
+   states={
+       NAME: [
+           CommandHandler('cancel', cancel),  # has to be before MessageHandler to catch `/cancel` as command, not as `name`
+           MessageHandler(Filters.text, get_name)
+       ],
+       AGE: [
+           CommandHandler('cancel', cancel),  # has to be before MessageHandler to catch `/cancel` as command, not as `age`
+           MessageHandler(Filters.text, get_age)
+       ],
+       GENDER: [
+           CommandHandler('cancel', cancel),  # has to be before MessageHandler to catch `/cancel` as command, not as `gender`
+           MessageHandler(Filters.regex('^(Male|Female)$'), get_gender)
+       ],
+       ATTENDING: [
+           CommandHandler('cancel', cancel),  # has to be before MessageHandler to catch `/cancel` as command, not as `attending`
+           MessageHandler(Filters.regex('^(Yes|No)$'), get_gender)
+       ],
+   },
+   fallbacks=[CommandHandler('cancel', cancel)]
+)                
+
 # this function replies to text sent to the bot that are not commands
 def reply(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Okay thank you. Type /start to begin or /cancel when you are done.')
 reply_handler = MessageHandler(Filters.text & (~Filters.command), reply)
-# this function handles the cancel command
-def cancel(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text(
-        'Bye! I hope we can talk again some day.'
-    )
-cancel_handler = CommandHandler('cancel', cancel)
 # this function handles any unknown command
 def unknown(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Sorry, I didn't understand that command. \n"
                              'Please type the correct one.')
 
 unknown_handler = MessageHandler(Filters.command, unknown)
+
 updater = Updater(Token)
 # run the start function
-updater.dispatcher.add_handler(start_handler)
-# run the rsvp function
-updater.dispatcher.add_handler(rsvp_handler)
+updater.dispatcher.add_handler(my_conversation_handler)
 # run the relpy function
 updater.dispatcher.add_handler(reply_handler)
-# run the cancel function
-updater.dispatcher.add_handler(cancel_handler)
 # run the unknown function
 updater.dispatcher.add_handler(unknown_handler)
 # Connect to Telegram and wait for messages
